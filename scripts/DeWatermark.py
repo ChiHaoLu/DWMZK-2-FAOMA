d3add2f1b506783f70b133f2af1e6d390f6c6979
chen-yanlong <alan900801@gmail.com>
2023-05-31 13:42:59 +0800

    fix watermark

--- scripts/DeWatermark.py
+++ scripts/DeWatermark.py
@@ -1,8 +1,12 @@
 import cv2
 from imwatermark import WatermarkDecoder
 
+# import os
+# os.chdir(r'<path to test_w.png>')
+
+
 bgr = cv2.imread(r'test_w.png')
 
-decoder = WatermarkDecoder('bytes', 32)
-watermark = decoder.decode(bgr, 'dwtDct')
+decoder = WatermarkDecoder('bytes', 344) # 344 means the bytes number of the encoded message.
+watermark = decoder.decode(bgr, 'dwtDctSvd')
 print(watermark.decode('utf-8'))
