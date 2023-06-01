99a3118874b402cbd89e9f3dcc035ae94a9e4f5e
chen-yanlong <alan900801@gmail.com>
2023-06-01 13:09:47 +0800

    add image

--- scripts/DeWatermark.py
+++ scripts/DeWatermark.py
@@ -4,9 +4,7 @@ from imwatermark import WatermarkDecoder
 # import os
 # os.chdir(r'<path to test_w.png>')
 
-
-bgr = cv2.imread(r'test_w.png')
-
-decoder = WatermarkDecoder('bytes', 344) # 344 means the bytes number of the encoded message.
+bgr = cv2.imread(r'../image/test_w.png')
+decoder = WatermarkDecoder('bytes', 344) # the bytes number of the encoded message.
 watermark = decoder.decode(bgr, 'dwtDctSvd')
 print(watermark.decode('utf-8'))
--- scripts/Watermark.py
+++ scripts/Watermark.py
@@ -1,24 +1,15 @@
 import cv2 
 from imwatermark import WatermarkEncoder
-from PIL import Image
-import numpy as np
+
 # import os
 # os.chdir(r'<path to test.png>')
 
-bgr = cv2.imread(r'test.png')
-# wm = 'hello my friend'
-wm = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+bgr = cv2.imread(r'../image/test.png')
+# words to be encode
+wm = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' 
 
 encoder = WatermarkEncoder()
 encoder.set_watermark('bytes', wm.encode('utf-8'))
 bgr_encoded = encoder.encode(bgr, 'dwtDctSvd')
 
-cv2.imwrite('test_w.png', bgr_encoded)
-
-
-# def put_watermark(img, wm_encoder=None):
-#     if wm_encoder is not None:
-#         img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-#         img = wm_encoder.encode(img, 'dwtDct')
-#         img = Image.fromarray(img[:, :, ::-1])
-#     return img
+cv2.imwrite(r'../image/test_w.png', bgr_encoded)
